e2bd005c980cafb9637bb4370fbfdb2c949f7916
megumidaisiki <124884408+megumidaisiki@users.noreply.github.com>
2025-04-21 19:28:17 +0800

    Delete app directory

--- app/config.py
+++
@@ -1,5 +0,0 @@
-import os
-
-class Config:
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:123456@localhost:3306/hospital_data' # 更改成你自己的 username:password@localhost:host/databasename
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
